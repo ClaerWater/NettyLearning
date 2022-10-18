872b206edd00067a5e7b32b598501d9439df4c11
whiterose <2975804613@qq.com>
2022-10-18 14:14:35 +0800

    first commit

---
+++ cloud-api-commons/src/main/java/com/sjx/springcloud/entities/GitTest.java
@@ -0,0 +1,12 @@
+package com.sjx.springcloud.entities;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+
+    public void m1(){
+        System.out.println();
+    }
+}
