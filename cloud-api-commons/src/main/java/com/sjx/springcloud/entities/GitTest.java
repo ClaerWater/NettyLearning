9997e2265145621938134a97c3c49fe0ae4a97f8
whiterose <2975804613@qq.com>
2022-10-18 14:26:05 +0800

    hot-fix commit1

--- cloud-api-commons/src/main/java/com/sjx/springcloud/entities/GitTest.java
+++ cloud-api-commons/src/main/java/com/sjx/springcloud/entities/GitTest.java
@@ -3,6 +3,9 @@ package com.sjx.springcloud.entities;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 
 
